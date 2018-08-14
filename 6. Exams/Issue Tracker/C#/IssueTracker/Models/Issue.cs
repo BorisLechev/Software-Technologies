8eca39bd88a89a161bfc7dc2079962ffdfff0f54
BorisLechev <b.k.lechev@gmail.com>
2018-08-14 22:48:50 +0300

    Added exam and .docx file.

---
+++ 6. Exams/Issue Tracker/C#/IssueTracker/Models/Issue.cs
@@ -0,0 +1,15 @@
+﻿namespace IssueTracker.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Issue
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Priority { get; set; }
+    }
+}
---
+++ 6. Exams/Issue Tracker/Java/src/main/java/issuetracker/bindingModel/IssueBindingModel.java
@@ -0,0 +1,34 @@
+package issuetracker.bindingModel;
+
+public class IssueBindingModel {
+
+    private String title;
+
+    private String content;
+
+    private String priority;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
